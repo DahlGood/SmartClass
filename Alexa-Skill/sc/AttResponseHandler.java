5ab7cda901537ea4846108d52e46fefb60365aae
Luke <lukedependahl@aol.com>
2018-11-26 11:13:24 -0500

    Implemented queue service

--- Alexa-Skill/sc/AttResponseHandler.java
+++ Alexa-Skill/sc/AttResponseHandler.java
@@ -3,6 +3,14 @@ package sc;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 
 import java.util.Optional;
 
@@ -17,6 +25,18 @@ public class AttResponseHandler implements RequestHandler {
     //Response given to user.
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
+
+        AWSCredentials credentials = new BasicAWSCredentials("AKIAIP6FJXLSG6ZSKPRA", "MnjNItNpmOVLRMAaK7aSsLOTFRAaUJffntrWwh0/");
+        AmazonSQS sqs = AmazonSQSClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_1).build();
+        CreateQueueRequest createQueueRequest = new CreateQueueRequest("ScriptStarter");
+        String url = sqs.createQueue(createQueueRequest).getQueueUrl();
+
+        String message = "Test from skill 2";
+
+        SendMessageRequest sendMessageStandardQueue = new SendMessageRequest().withQueueUrl(url).withMessageBody(message).withDelaySeconds(30);
+
+        sqs.sendMessage(sendMessageStandardQueue);
+
 
         String response = "Please look ahead... taking attendance.";
 
